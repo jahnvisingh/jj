5fdd317078a74dab954add30bc539e74ee47dd64
jahnvisingh <jahnvisingh.1196@gmail.com>
2017-06-11 19:48:25 +0530

    Add files via upload

--- my.js
+++ my.js
@@ -3,24 +3,42 @@ $(document).ready(function(){
  var counter=0;
  var all=new Array();
  var fun=function()
-   {
-   $(this).css({"background-color":"white","text-align":"center","font-size":"50px"}).html(selectletter[Math.floor(Math.random() * 16)]);
-   all[counter]=$(this).text();
-   counter++;
-      if(counter==2)
-        { if(all[0]==all[1])
+   { 
+     $(this).css({"background-color":"white","text-align":"center","font-size":"50px"}).html(selectletter[Math.floor(Math.random()*16)]);
+     
+     all[counter]=$(this);
+     counter++;
+       if(counter==2)
+        { if(all[0].text()==all[1].text())
     	   {  
-             
-             alert("success!!");
-           }
+              
+              setTimeout(function(){
+    	   	  all[0].css({"font-size":"0px"});
+              
+              all[1].css({"font-size":"0px"});
+
+              alert("success!!");
+              },1000);
+              
+
+          }
           else
            { 
-             
-       	     alert("failure!!");
+           	 
+             setTimeout(function(){
+           	 all[0].css({"font-size":"0px","background-color":"#d3d3d3"});
+           	 
+             all[1].css({"font-size":"0px","background-color":"#d3d3d3"});
+
+             alert("failure!!");
+             },1000);
+            
+
        	   }
-       }
-   }
- $("td").on('click',fun);
-    
+         }
+  }
+
+  $("td").on('click',fun);
+   
 
 });
