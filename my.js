6c6f736a36cc74275cbd8ff558692032e323045d
jahnvisingh <jahnvisingh.1196@gmail.com>
2017-06-14 15:06:25 +0530

    Add files via upload

--- my.js
+++ my.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+var counter=0;
+var turns=0;
+var count=0;
 var all=new Array();
 var letter=["A","A","B","B","C","C","D","D","E","E","F","F","G","G","H","H"];
 
@@ -17,34 +20,41 @@ letter.shuffle();
 
 letter.forEach(function(item,index){
    
-   $("#"+index).click(function(){ $(this).css({"background-color":"white","text-align":"center","font-size":"50px"}).text(item);
-                                all[counter]=$(this);
-	                            counter++; 
-
-	          if(counter==2)
-
-        {  
+   $("#"+index).click(function(){ $(this).css({"background-color":"white","text-align":"center","font-size":"50px","font-family":"Comic Sans MS",
+   	                                         "font-weight":"bold"}).text(item);
+   	                              all[counter]=$(this);
+	                              counter++; 
+	             if(counter==2)
+	      { 
           if(all[0].text()==all[1].text())
     	   {  setTimeout(function(){
     	   	  all[0].css({"font-size":"0px"});
               all[1].css({"font-size":"0px"});
-              alert("success!!");
               },1000);
-    	     count++;}
+    	      count++;
+    	   }
           else
            { setTimeout(function(){
-           	 all[0].css({"font-size":"0px","background-color":"#d3d3d3"});
-           	 all[1].css({"font-size":"0px","background-color":"#d3d3d3"});
-           	 alert("failure!!");
+           	 all[0].css({"font-size":"0px","background-color":"#8A2BE2"});
+           	 all[1].css({"font-size":"0px","background-color":"#8A2BE2"});
              },1000);
            }
+          turns++;
           counter=0;
-        }
-
-	                            });
-});
+        }document.getElementById("turns").innerHTML="TURNS"+"="+turns;
+        document.getElementById("result").innerHTML="PAIRED CARDS FOUND"+"="+count;
+        if(count==8)
+        document.getElementById("win").innerHTML="GAME FINISHED!!";
+               
+	      });
  
+});
 
+
+ 
+$("button").click(function(){
+  location.reload();
+})
 
  
 
